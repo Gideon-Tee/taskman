0b534819837764d446530e131df5c84cf1effa30
Gideon Tettehfio <gideonlambride@gmail.com>
2025-01-09 18:14:55 +0000

    dynamic navbar features

---
+++ navbar.js
@@ -0,0 +1,30 @@
+const authToken = localStorage.getItem('authToken');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nav_logout = document.getElementById('nav-item-logout');
+
+    if (authToken) {
+        const nav_login = document.getElementById('nav-item-login');
+        nav_login.innerHTML = '';
+        const nav_signup = document.getElementById('nav-item-signup');
+        nav_signup.innerHTML = '';
+        
+    } else {
+        nav_logout.innerHTML = "";
+    }
+
+    nav_logout.addEventListener('click', (e) => {
+        e.preventDefault();
+        const confirmation = confirm('Are you sure you want to logout?');
+        
+        if (confirmation) {
+            localStorage.removeItem('authToken');
+            window.location.href='login.html';
+
+        } else {return;}
+
+    })
+
+
+})
